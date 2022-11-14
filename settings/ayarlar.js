71509dcb1181f4e94d91a7482a439f59779f57a1
pyrzzzzz <118159974+pyrzzzzz@users.noreply.github.com>
2022-11-14 10:38:38 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "MTA0MTM0NzI5OTg4NTk3MzUzNA.Ga2M2T.PGkaD1ZWubvObX5QZw2t4n_Y8ilfs2jLTgciFE
+",  // bot token gir :)
     PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    SAHİP: process.env.SAHİP || "950138394397257819", //sahip id :()
 }
